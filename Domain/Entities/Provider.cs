ecf28d5224efd220218e77e3a8fd29592702cae0
Mauricio Porras <mauricio.porras@techsbcn.com>
2024-09-28 16:18:49 -0500

    Updating dependencies

--- Domain/Entities/Provider.cs
+++ Domain/Entities/Provider.cs
@@ -2,5 +2,11 @@
 {
     public class Provider : BaseEntity
     {
+        public string? Name { get; set; }
+        public string? Nit { get; set; }
+        public string? Email { get; set; }
+        public Dictionary<string, string>? PersonalizedFields { get; set; }
+        public virtual ICollection<Service>? Services { get; set; }
+
     }
 }
---
+++ Domain/Entities/Service.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    public class Service : BaseEntity
+    {
+        public string? Name { get; set; }
+        public decimal? PriceByHour { get; set; }
+        public virtual ICollection<Country>? Countries { get; set; }
+    }
+}
